245d4de1a043b52f1202feb9843a573294f47af4
Neon-soN1k <Kustikbitch@yandex.ru>
2025-12-05 15:00:50 +0300

    Update 6.py

--- 6.py
+++ 6.py
@@ -1,1 +1,18 @@
-
+#Задание1
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+print("matrix:")
+for row in matrix:
+    print(row)
+odd_numbers = []
+for row in matrix:
+    for item in row:
+        if item % 2 != 0:
+            odd_numbers.append(str(item))
+print("\nнечётные числа matrix", " ".join(odd_numbers))
+even_count = 0
+for row in matrix:
+    for item in row:
+        if item % 2 == 0:
+            even_count += 1
+print(f"\nкол-во чётных: {even_count}")
+#Задание2
