d2c5c7e120079468bf57540ae030e1b6f085dabe
Neon-soN1k <Kustikbitch@yandex.ru>
2025-12-12 13:30:37 +0300

    Update 7.py

--- 7.py
+++ 7.py
@@ -38,3 +38,68 @@ for row in data:
 
 print("\n--- Все файлы .js с префиксом new_ ---")
 print(new_js_files)
+#Задание2
+word_numb = ["ноль", "один", "два", "три", "четыре", "пять",
+"шесть", "семь", "восемь", "девять"]
+try:
+    n = int(input("Введите число n от 0 до 9: "))
+    if 0 <= n <= 9:
+        print(f"Элементы списка от первого до {n}-го (включительно):")
+        for i in range(n + 1):
+            print(word_numb[i])
+    else:
+        print('Введите число <= 9')
+except ValueError:
+    print("Ошибка ввода. Пожалуйста, введите целое число.")
+#Задание3
+bin_sy = ['11011111', '11011101', '11000111', '11011100', '11011110']
+decimal_numbers = []
+print("Десятичные представления элементов списка:")
+for binary_str in bin_sy:
+    decimal_num = int(binary_str, 2)
+    decimal_numbers.append(decimal_num)
+    print(decimal_num)
+max_num = max(decimal_numbers)
+min_num = min(decimal_numbers)
+print(f"\nМаксимальное число: {max_num}")
+print(f"Минимальное число: {min_num}")
+#Задание4
+A = [
+    [-446, 281, -80],
+    [465, 432, -122],
+    [13, 'error', 8]
+]
+print(f"Исходная матрица A:\n{A}\n")
+for row_index, row in enumerate(A):
+    for col_index, element in enumerate(row):
+        if isinstance(element, str):
+            replacement_value = len(element)
+            A[row_index][col_index] = replacement_value
+            print(f"Элемент '{element}' по адресу [{row_index}][{col_index}] заменен на число: {replacement_value}")
+print(f"\nИзмененная матрица A:\n{A}\n")
+total_sum = 0
+for row in A:
+    for element in row:
+        total_sum += element
+print(f"Сумма всех элементов матрицы: {total_sum}")
+#Задание5
+matrix = [
+    [10, 2, 3],
+    [4, 50, 6],
+    [7, 8, 90]
+]
+print(f"Исходная матрица:\n{matrix}\n")
+diagonal_sum = 0
+for i in range(len(matrix)):
+    diagonal_element = matrix[i][i]
+    diagonal_sum += diagonal_element
+    print(f"Добавляем элемент [{i}][{i}]: {diagonal_element}")
+print(f"\nСумма чисел по главной диагонали: {diagonal_sum}")
+secondary_diagonal_sum = 0
+n = len(matrix) # Размер матрицы (3)
+print("\n--- Расчет побочной диагонали ---")
+for i in range(n):
+    element = matrix[i][n - 1 - i]
+    secondary_diagonal_sum += element
+    print(f"Добавляем элемент [{i}][{n - 1 - i}]: {element}")
+print(f"\nСумма чисел по побочной диагонали: {secondary_diagonal_sum}")
